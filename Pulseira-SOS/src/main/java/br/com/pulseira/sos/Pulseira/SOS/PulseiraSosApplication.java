c0e5b90dd4c0820e0802fbaa0cf824bd21d6fb74
luan441 <luanhenrique441@gmail.com>
2023-08-17 21:09:33 -0300

    first commit

---
+++ Pulseira-SOS/src/main/java/br/com/pulseira/sos/Pulseira/SOS/PulseiraSosApplication.java
@@ -0,0 +1,13 @@
+package br.com.pulseira.sos.Pulseira.SOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseiraSosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseiraSosApplication.class, args);
+	}
+
+}
---
+++ Pulseira-SOS/src/test/java/br/com/pulseira/sos/Pulseira/SOS/PulseiraSosApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.pulseira.sos.Pulseira.SOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseiraSosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
