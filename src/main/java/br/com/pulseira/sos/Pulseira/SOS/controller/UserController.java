61ca302b14b7b75ad67dd256a5adfbd9774f5a8d
luan441 <luanhenrique441@gmail.com>
2023-10-27 10:21:33 -0300

    chore: configurando swagger

--- src/main/java/br/com/pulseira/sos/Pulseira/SOS/controller/UserController.java
+++ src/main/java/br/com/pulseira/sos/Pulseira/SOS/controller/UserController.java
@@ -1,11 +1,10 @@
 package br.com.pulseira.sos.Pulseira.SOS.controller;
 
 
-import br.com.pulseira.sos.Pulseira.SOS.model.*;
 import br.com.pulseira.sos.Pulseira.SOS.persistence.entity.*;
 import br.com.pulseira.sos.Pulseira.SOS.service.*;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.*;
-import org.apache.catalina.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +19,8 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @GetMapping(path = "/get-all")
+    @Operation(description = "Buscar todos os usuários", tags = "Usuário", summary = "Buscar todos os usuários")
     public ResponseEntity<List<UserEntity>> getAll(){
         return ResponseEntity.ok(service.getAll());
     }
